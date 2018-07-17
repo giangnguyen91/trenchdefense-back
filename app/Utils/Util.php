32ae89a71bf9cdc0e2f67695b881529c6d9975cc
Nguyen Truong Giang <nguyentruonggiang91@gmail.com>
2018-07-17 22:00:09 +0700

    + add toProbufMacro

---
+++ app/Utils/Util.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Utils;
+
+use App\Exceptions\IllegalTypeObjectException;
+use \DrSlump\Protobuf\Message;
+use App\Models\ProbufMessages;
+use App\Models\ProtobufMessage;
+
+class Util
+{
+    /**
+     *
+     * @param  array|Message[]  $objects
+     * @return string
+     */
+    public static function serialize(array $objects)
+    {
+        $messages = new ProbufMessages();
+        $count = 0;
+        foreach($objects as $object) {
+            if(!($object instanceof Message)) throw new IllegalTypeObjectException("can not serializable object");
+            $message = new ProtobufMessage();
+            $typeId = TypeTableUtil::objectToTypeId($object);
+            $message->setType($typeId);
+            $message->setPayload($object->serialize());
+            $messages->addMessages($message);
+            $count++;
+        }
+        $messages->setCount($count);
+        return $messages->serialize();
+    }
+
+    /**
+     *
+     * @param  string $serializedObject
+     * @return array|Message[]
+     */
+    public static function deserialize($serializedObject)
+    {
+        $objects = [];
+        $messages = ProbufMessages::deserialize($serializedObject);
+        for($i=0; $i<$messages->getCount(); $i++) {
+            $message = $messages->getMessages($i);
+            $object = TypeTableUtil::typeIdToObject($message->getType());
+            $object = $object->deserialize($message->getPayload());
+            $objects[] = $object;
+        }
+        return $objects;
+    }
+}
