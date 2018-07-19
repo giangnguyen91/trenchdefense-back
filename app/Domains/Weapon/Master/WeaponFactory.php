56f398700f17f14550504eb39f6353baddab4ce3
Nguyen Truong Giang <giang.nguyen@vn.devinition.com>
2018-07-19 14:29:38 +0700

    + add delay time

--- app/Domains/Weapon/Master/WeaponFactory.php
+++ app/Domains/Weapon/Master/WeaponFactory.php
@@ -6,23 +6,27 @@ class WeaponFactory
 {
 
     /**
+     * @param WeaponId $weaponId
      * @param Damage $damage
      * @param ReloadSpeed $reloadSpeed
      * @param ShotSpeed $shotSpeed
+     * @param DelayTime $delayTime
      * @return Weapon
      */
     public function make(
         WeaponId $weaponId,
         Damage $damage,
         ReloadSpeed $reloadSpeed,
-        ShotSpeed $shotSpeed
+        ShotSpeed $shotSpeed,
+        DelayTime $delayTime
     ): Weapon
     {
         return new Weapon(
             $weaponId,
             $damage,
             $reloadSpeed,
-            $shotSpeed
+            $shotSpeed,
+            $delayTime
         );
     }
 
@@ -39,7 +43,8 @@ class WeaponFactory
             new WeaponId($weapon->id),
             new Damage($weapon->damage),
             new ReloadSpeed($weapon->reload_speed),
-            new ShotSpeed($weapon->shot_speed)
+            new ShotSpeed($weapon->shot_speed),
+            new DelayTime($weapon->delay_time)
         );
     }
 }
