ba53925f76a86cdcfc14a4b95232c03eda7fc6b9
Nguyen Truong Giang <giang.nguyen@vn.devinition.com>
2018-09-04 17:51:56 +0700

    + story 11

---
+++ app/Domains/Wave/WaveRepository.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Domains\Wave;
+
+use Illuminate\Support\Collection;
+
+class WaveRepository
+{
+    /**
+     * @var WaveFactory
+     */
+    private $waveFactory;
+
+    /**
+     * @param WaveFactory $waveFactory
+     */
+    public function __construct(
+        WaveFactory $waveFactory
+    )
+    {
+        $this->waveFactory = $waveFactory;
+    }
+
+    /**
+     * @param WaveID $waveID
+     * @return Wave | null
+     */
+    public function find(
+        WaveID $waveID
+    ): ?Wave
+    {
+        $waveEloquent = \App\Wave::query()->where('id', $waveID->getValue())->first();
+
+        if (is_null($waveEloquent)) return null;
+        return $this->waveFactory->makeByEloquent($waveEloquent);
+    }
+
+    /**
+     * @param Wave $wave
+     * @return WaveID
+     */
+    public function persist(
+        Wave $wave
+    ): WaveID
+    {
+        $eloquent = \App\Zombie::unguarded(function () use ($wave) {
+            return \App\Zombie::query()->updateOrCreate(
+                [
+                    'id' => !is_null($wave->getId()->getValue()) ? $wave->getId()->getValue() : null
+                ],
+                [
+                    'name' => $wave->getName()->getValue()
+                ]
+            );
+        });
+
+        return new WaveID($eloquent->id);
+    }
+
+    /**
+     * @return Wave[] | Collection
+     */
+    public function all(): Collection
+    {
+        $waveEloquents = \App\Wave::query()->get();
+        return collect($waveEloquents)->map(function (\App\Wave $eloquent) {
+            return $this->waveFactory->makeByEloquent($eloquent);
+        });
+    }
+
+    /**
+     * @param Wave $wave
+     * @return mixed
+     */
+    public function remove(Wave $wave)
+    {
+        \App\Wave::destroy($wave->getID()->getValue());
+    }
+}
