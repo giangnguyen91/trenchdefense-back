11498f5a04b689d623fa614cda9a373a80dbb80f
Luan Tran <luan.tran@vn.devinition.com>
2018-09-25 18:04:01 +0700

    add in AccessToken proto and RequestAccessCodeParameter proto

--- app/Http/Controllers/GameUserController.php
+++ app/Http/Controllers/GameUserController.php
@@ -9,12 +9,15 @@
 namespace App\Http\Controllers;
 
 
+use App\Components\Auth\AuthComponent;
 use App\Domains\Auth\TokenRepository;
 use App\Domains\User\GameUser;
 use App\Domains\User\GameUserFactory;
 use App\Domains\User\GameUserRepository;
 use App\Domains\User\Imei;
+use App\Proto\AccessCode;
 use Illuminate\Http\Request;
+use App\Proto\RequestAccessTokenParameter;
 
 class GameUserController extends Controller
 {
@@ -33,6 +36,11 @@ class GameUserController extends Controller
      */
     private $tokenRepository;
 
+    /**
+     * @var AuthComponent
+     */
+    private $authComponent;
+
     public function __construct(
         GameUserRepository $gameUserRepository,
         GameUserFactory $gameUserFactory,
@@ -46,19 +54,19 @@ class GameUserController extends Controller
 
     public function createAccessToken(Request $request)
     {
-        $imei = $request->get('imei');
-        $gameUser =  $this->gameUserRepository->findByIMEI(new Imei($imei));
+        $requestAccessTokenParameter = $request->get(RequestAccessTokenParameter::class);
+        $gameUser =  $this->gameUserRepository->findByIMEI(new Imei($requestAccessTokenParameter->imei));
         if(is_null($gameUser)){
-            $gameUser = $this->createNewGameUser($imei);
+            $gameUser = $this->createNewGameUser($requestAccessTokenParameter->imei);
         }
         $token = $this->tokenRepository->createTokenForGameUser($gameUser);
-        $data = [
-            'id' => $token->getTokenID()->getValue(),
-            'game_user_id' => $token->getGameUserID()->getValue(),
-            'access_token' => $token->getAccessToken()->getValue()
 
-        ];
-        return response()->json($data);
+        $accessCode = new AccessCode();
+        $accessCode->tokenID = $token->getTokenID()->getValue();
+        $accessCode->gameUserID = $token->getGameUserID()->getValue();
+        $accessCode->token = $token->getAccessToken()->getValue();
+
+        return response()->protobuf([$accessCode]);
     }
 
     private function createNewGameUser(string $imei): GameUser
@@ -70,8 +78,9 @@ class GameUserController extends Controller
 
     public function getInfo(Request $request)
     {
-        $loggedInUser = app('auth')->guard()->user();
-        return response()->json(['loggedinuserid' => "xxx"]);
+        $loggedInUserID = $this->authComponent->getGameUserId();
+        $gameUser = $this->gameUserRepository->findByID($loggedInUserID);
+        return response()->protobuf([$gameUser->toProtobuf()]);
     }
 
 }
--- app/Utils/ProtobufObject.php
+++ app/Utils/ProtobufObject.php
@@ -5,6 +5,8 @@ use \DrSlump\Protobuf\Message;
 use App\Proto\Error;
 use App\Proto\ProtobufMessage;
 use App\Proto\ProtobufMessages;
+use App\Proto\AccessCode;
+use App\Proto\Imei;
 use App\Proto\Character;
 use App\Proto\HavingCharacter;
 use App\Proto\UpdateSettingParameter;
@@ -30,6 +32,8 @@ class ProtobufObject
                         case $object instanceof Error: return 0x7f2f6a15;
             case $object instanceof ProtobufMessage: return 0xe5bba9f8;
             case $object instanceof ProtobufMessages: return 0xfb72b96d;
+            case $object instanceof AccessCode: return 0xebb88223;
+            case $object instanceof Imei: return 0xb4589e46;
             case $object instanceof Character: return 0xee9946c8;
             case $object instanceof HavingCharacter: return 0xecb02e52;
             case $object instanceof UpdateSettingParameter: return 0x4cbf68cf;
@@ -56,6 +60,8 @@ class ProtobufObject
                         case 0x7f2f6a15: return new Error();
             case 0xe5bba9f8: return new ProtobufMessage();
             case 0xfb72b96d: return new ProtobufMessages();
+            case 0xebb88223: return new AccessCode();
+            case 0xb4589e46: return new Imei();
             case 0xee9946c8: return new Character();
             case 0xecb02e52: return new HavingCharacter();
             case 0x4cbf68cf: return new UpdateSettingParameter();
