6b4b26338ebc20cb68c53c2bb94ea4bb9f841687
Nguyen Truong Giang <giang.nguyen@vn.devinition.com>
2018-09-27 10:52:29 +0700

    + authenticate

--- app/Components/Auth/AuthComponent.php
+++ app/Components/Auth/AuthComponent.php
@@ -2,29 +2,19 @@
 
 namespace App\Components\Auth;
 
-use App\Domains\User\GameUser;
 use App\Domains\User\GameUserID;
-use Illuminate\Auth\AuthManager;
 
 class AuthComponent
 {
-    public function __construct(AuthManager $manager)
-    {
-        $this->guard = $manager->guard();
-    }
-
     /**
      * @return GameUserID | null
      */
     public function getGameUserId(): ?GameUserID
     {
-        if (\Config::get("debug.force_login") && \Config::get("app.env") != "production") {
-            $gameUserID = new GameUserID(\Config::get("debug.user_id"));
-            return $gameUserID;
-        }
-        if (!$this->guard->check()) {
+        $user = request()->user();
+        if (is_null($user)) {
             return null;
         }
-        return new GameUserID($this->guard->id());
+        return new GameUserID($user->id);
     }
 }
